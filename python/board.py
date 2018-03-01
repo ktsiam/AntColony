8e67d6a96c5c4ca053e6f21b9af0e622b7b6b11e
ktsiam <ktsiampouris@gmail.com>
2018-02-28 23:36:43 -0500

    totally not tested, but still modular board.py ;)

--- python/board.py
+++ python/board.py
@@ -1,183 +1,139 @@
 import numpy as np
 import math as m
 
-DIM = 100
-INV_DIM = 1 / DIM
-NUM_ANTS = 1
 
-LOOK_RNG = 1
-LOOK_N = LOOK_RNG ** 2
+DIM      = 100
+NUM_ANTS = 10
+OBS_LEN  = 3
 
-## Tile field enums
-TILE_LEN = 1
-T_FOOD = 0
-# T_TRAIL = 1
+class Coord(object): #Coord object
+    def __init__(self, _x = 0, _y = 0):
+        self.x = _x
+        self.y = _y
 
-OBS_LEN = 3 # l,f,r food
-O_FOOD = 0
-# O_Trail = 9
-
-ACT_LEN = 2
-A_LIN = 0
-A_ROT = 1
-
-SPLIT_FOOD = 600
+    def __add__(self, c):
+        self.x += c.x
+        self.y += c.y
 
 
-class Board(object):
-    def __init__(self, dim=DIM, num_ants=NUM_ANTS):
-        self.dim = dim
-        self.num_ants = num_ants
-        self.rot_arr = np.array([-m.pi/3, 0, m.pi/3])
+        
+class Tile(object): #Tile object
+    GROWTH_RATE     = 0.1
+    INIT_FOOD_RATIO = 0.5
+    PEAK_STRENGTH   = [60, 70, 90]
+    PEAK_TIGHTNESS  = [1.0, 3.5, 1.0]
+    PEAK_COORD      = [Coord(3*DIM/4, 2*DIM/3), Coord(8*DIM/9, DIM/9),
+                      Coord(  DIM/4,   DIM/3)]
+    
+    def __init__(self, x, y):
+        
+        self.food_cap = 0
+        for i in range(len(PEAK_STRENGTH)):
+            food_cap += set_food_cap(Coord(x, y), PEAK_COORD[i],
+                                     PEAK_STRENGTH[i], PEAK_TIGHTNESS[i])
+        self.food = food_cap * INIT_FOOD_RATIO
 
-        self.gen_coord = (DIM/4, DIM/(3))
+    def grow(self):
+        reg   = (1 - self.food / max (self.food_cap, 0.000001))
+        noise = np.random.normal(1, 0.2)
+        self.food += GROWTH_RATE * noise * reg
 
-        self.targets = np.zeros((DIM,DIM),dtype=float)
-        for x in range(DIM):
-            for y in range(DIM):
-                self.targets[x][y] = self.calc_targets(x,y,90)
+    def get_eaten(self):
+        self.food /= 2
+        if  self.food > 20.0:
+            self.food -= 5
+        
 
-        self.gen_coord = (3*DIM/4, 2*DIM/(3))
-        for x in range(DIM):
-            for y in range(DIM):
-                self.targets[x][y] += self.calc_targets(x,y,60)
-
-        self.gen_coord = (DIM - (DIM/9), DIM/(9))
-        for x in range(DIM):
-            for y in range(DIM):
-                self.targets[x][y] += self.calc_targets(x,y,70, 3.5)
-
-        start = np.random.normal(0.7, 0.01,(DIM,DIM))
-        self.food = self.targets.copy() * start
-
-
-        self.obs   = np.zeros((num_ants, OBS_LEN),dtype=float)
-        self.id_ant_count = NUM_ANTS
-        self.Ants  = [Ant(_ID=i) for i in range(NUM_ANTS)]
-
-
-    def calc_targets(self, x,y, strength=100, tightness=1.0):
-        """
-        Calculates upper bounds for food per tile using a 2D normal dist
-
-        for larger spread make exp_const smaller (ie less food near edges)
-        """
-
-        x_1, y_1 = self.gen_coord
-        exp_const = INV_DIM * 0.035 * tightness
-        exp = exp_const * (-(x-x_1) ** 2 - (y-y_1) ** 2)
+        
+    @staticmethod
+    def set_cap(p, _p, strength, tightness):
+        exp_const = 0.035 * tightness / DIM
+        exp   = exp_const * (-(p.x - _p.x) ** 2 - (p.y - _p.y) ** 2)
         noise = np.random.normal(1, 0.05)
-        if noise < 0.3:
-            noise = 0.3
+        
+        return noise if noise > 0.3 else 0.3
 
-        return strength  *  (2 ** exp) * noise
+    
 
+class Ant(object): # Ant object
+    SPLIT_FOOD = 600
+    A_ROT      = 1 # no idea what this is btw
+    ROT_ARR    = [-m.pi/3, 0, m.pi/3]
+    def __init__(self, _pos = Coord(DIM/2, DIM/2), _theta=0, _food=50):
 
-    def growth_food(self, rate=0.10):
-        self.food[self.food < 0] = 1
+        self.theta = _theta # [0, 2π]
+        self.food  = _food
+        self.pos   = _pos + Coord(np.random.normal(0, DIM/20),
+                                  np.random.normal(0, DIM/20))
+        self.coord = Coord(int(self.pos.x), int(self.pos.y))
 
-        reg = (1 - (self.food)/(self.targets+0.0001))
-        reg = self.targets * reg
-        noise = np.random.normal(1,0.2)
+    def act(self, action, tiles): 
+        # action : 0, 1, 2 (left, front, right)
+        dTheta       = (action[A_ROT] - 1)      *  m.pi / 3
+        self.theta   = (dTheta +    self.theta) % (m.pi * 2)
+        self.pos.x  += dist * m.cos(self.theta) %  DIM
+        self.pos.y  += dist * m.sin(self.theta) %  DIM
+        self.coord.x =          int(self.pos.x)
+        self.coord.y =          int(self.pos.y)
 
-        return rate * noise * reg
+        # food : ant, square
+        self.eat(tiles[self.coord.x][self.coord.y].food)
+        tiles         [self.coord.x][self.coord.y].get_eaten()
 
-    def get_obs(self, index, ant):
-        """ obs - shape (NUM_ANTS, OBS_LEN)"""
-        obs = self.obs[index]
-        new_thetas = (ant.theta + self.rot_arr) % (2*m.pi)
+    def eat(self, amount):
+        half = amount / 2
+        self.food += (half - 10) if half > 20.0 else (half - 5)
 
-        new_coords = [ (ant.x + 1.5*m.cos(ang), ant.y + 1.5*m.sin(ang)) for ang in new_thetas]
-        obs = np.array([self.food[int(x)][int(y)] for (x,y) in new_coords])
-        self.obs[index] = obs
+    def is_alive(self):
+        return self.food > 0
 
+    def is_reproducible(self):
+        return self.food > SPLIT_FOOD
 
-    def board_bounds(self, ant):
-        # ant.x = ((1 + ant.x) % (DIM - 3) - 1)
-        # ant.y = ((1 + ant.y) % (DIM - 3) - 1)
-        if ant.x > (DIM - 2):
-            ant.x -= 10
-        elif ant.x < (2):
-            ant.x += 10
-        if ant.y > (DIM - 2):
-            ant.y -= 10
-        elif ant.y < (2):
-            ant.y += 10
+    def observe(self, tiles):
+        new_thetas = (self.theta + ROT_ARR) % (2*m.pi)
+        new_xs     = int(self.pos.x + 1.5 * m.cos(new_thetas))
+        new_ys     = int(self.pos.y + 1.5 * m.sin(new_thetas))
+        obs        = [tiles[x][y] for (x, y) in zip(new_xs, new_ys)]
+        return obs
+    
+class Board(object):
+    def __init__(self, _ant_len = NUM_ANTS, gen_cord = Coord(DIM/4, DIM/3)):
+        self.ant_len = _ant_len
+        self.tiles   = [DIM][DIM]
+        
+        self.board = [DIM][DIM]
+        for x in range(DIM):
+            for y in range(DIM):
+                board[x][y] = Tile(x, y)
 
-    def update(self, action):
+        self.ants = [ANT_NB]
+        for i in range(ANT_NB):
+            ants[i] = Ant()
+        
+    def update(self, actions):
         """
         Args: action: np.ndarray((NUM_ANTS, ACT_LEN), dtype=float)
         actually ints
         """
-        self.food += self.growth_food(0.0005) # 0.005
+        self.tiles = self.tiles.grow()
 
+        for ant, action in zip(ants, actions):
+            ant.act(action, self.tiles)
 
-        for (i,ant) in enumerate(self.Ants):
-            dTheta = (action[i][A_ROT] - 1) * m.pi/3  ## action is 0, 1, 2 (l,f,r)
-            ant.theta = (ant.theta + dTheta) % (2 * m.pi)
-            dist = 1
+        # kill and make ants
+        self.ants     = filtertrue (Ant.is_alive, self.ants)
+        fat_ants       = filtertrue (Ant.is_reproducible, self.ants)
+        self.ants      = filterfalse(Ant.is_reproducible, self.ants)
+        fat_ants.food /= 2
+        fat_ants      += fat_ants # cloning
+        for ant in fat_ants:
+            ant.theta  = random.uniform(0, 2*m.pi)
+        self.ants     += fat_ants
+        
+        observations = [][OBS_LEN]
+        for ant in ants:
+            observations.append(ant.observe(self.tiles))
+            
+        return observations
 
-            ant.x += dist * m.cos(ant.theta)
-            ant.y += dist * m.sin(ant.theta)
-
-            self.board_bounds(ant)
-
-            (x,y) = (int(ant.x), int(ant.y))
-
-            tile_f = self.food[x][y]
-
-            ant.food += (tile_f / 2)
-            tile_f /= 2
-            if tile_f > 20.0:
-                tile_f -= 5.0
-                ant.food -= 10.0
-            else:
-                ant.food -= 5.0
-            self.food[x][y] = tile_f
-
-            # kill ant if it has no food
-
-
-        ant_list = []
-        ## split / kill
-        for (i,ant) in enumerate(self.Ants):
-            if ant.food > 0:
-                ant_list.append(ant)
-                if ant.food > SPLIT_FOOD:
-                    new_ant = Ant(ant.x, ant.y, np.random.uniform(0, m.pi * 2),
-                              _food = ant.food / 2)
-                    ant_list.append(new_ant)
-                    ant.food /= 2.0
-        self.Ants = ant_list
-        NUM_ANTS = len(self.Ants)
-
-        self.obs   = np.zeros((NUM_ANTS, OBS_LEN),dtype=float)
-        # get obs for each ants
-        # ensure ants within board
-        for (i,ant) in enumerate(self.Ants):
-            self.board_bounds(ant)
-            self.get_obs(i, ant)
-
-
-        return self.obs.copy()
-
-
-        @staticmethod
-        def check_angle(ang):
-            """ Ensures angle is between 0 and 2PI """
-            return ang % (2 * m.pi)
-
-class Ant(object):
-    def __init__(self, x_pos=DIM/2, y_pos=DIM/2, _theta=0, _ID=0, _food=50):
-        self.x = x_pos + np.random.normal(0, DIM/20)
-        self.y = y_pos + np.random.normal(0, DIM/20)
-        self.theta = _theta # 0 to 2pi
-
-
-
-        self.dx = 0
-        self.dy = 0
-
-        self.food = _food
-
-        self.ID = _ID
--- python/board2.py
+++ python/board2.py
@@ -21,6 +21,9 @@ ACT_LEN = 2
 A_LIN = 0
 A_ROT = 1
 
+SPLIT_FOOD = 600
+
+
 class Board(object):
     def __init__(self, dim=DIM, num_ants=NUM_ANTS):
         self.dim = dim
@@ -32,18 +35,28 @@ class Board(object):
         self.targets = np.zeros((DIM,DIM),dtype=float)
         for x in range(DIM):
             for y in range(DIM):
-                self.targets[x][y] = self.calc_targets(x,y)
+                self.targets[x][y] = self.calc_targets(x,y,90)
+
+        self.gen_coord = (3*DIM/4, 2*DIM/(3))
+        for x in range(DIM):
+            for y in range(DIM):
+                self.targets[x][y] += self.calc_targets(x,y,60)
+
+        self.gen_coord = (DIM - (DIM/9), DIM/(9))
+        for x in range(DIM):
+            for y in range(DIM):
+                self.targets[x][y] += self.calc_targets(x,y,70, 3.5)
 
         start = np.random.normal(0.7, 0.01,(DIM,DIM))
         self.food = self.targets.copy() * start
 
 
         self.obs   = np.zeros((num_ants, OBS_LEN),dtype=float)
+        self.id_ant_count = NUM_ANTS
+        self.Ants  = [Ant(_ID=i) for i in range(NUM_ANTS)]
 
-        self.Ants  = {i:Ant(_ID=i) for i in range(NUM_ANTS)}
 
-
-    def calc_targets(self, x,y):
+    def calc_targets(self, x,y, strength=100, tightness=1.0):
         """
         Calculates upper bounds for food per tile using a 2D normal dist
 
@@ -51,13 +64,13 @@ class Board(object):
         """
 
         x_1, y_1 = self.gen_coord
-        exp_const = INV_DIM * 0.025
+        exp_const = INV_DIM * 0.035 * tightness
         exp = exp_const * (-(x-x_1) ** 2 - (y-y_1) ** 2)
         noise = np.random.normal(1, 0.05)
         if noise < 0.3:
             noise = 0.3
 
-        return 100  *  (2 ** exp) * noise
+        return strength  *  (2 ** exp) * noise
 
 
     def growth_food(self, rate=0.10):
@@ -78,6 +91,7 @@ class Board(object):
         obs = np.array([self.food[int(x)][int(y)] for (x,y) in new_coords])
         self.obs[index] = obs
 
+
     def board_bounds(self, ant):
         # ant.x = ((1 + ant.x) % (DIM - 3) - 1)
         # ant.y = ((1 + ant.y) % (DIM - 3) - 1)
@@ -95,18 +109,18 @@ class Board(object):
         Args: action: np.ndarray((NUM_ANTS, ACT_LEN), dtype=float)
         actually ints
         """
-        self.food += self.growth_food(0.005)
+        self.food += self.growth_food(0.0005) # 0.005
 
-        for (i,ant) in enumerate(self.Ants.values()):
+
+        for (i,ant) in enumerate(self.Ants):
             dTheta = (action[i][A_ROT] - 1) * m.pi/3  ## action is 0, 1, 2 (l,f,r)
             ant.theta = (ant.theta + dTheta) % (2 * m.pi)
             dist = 1
 
-        ## FLOATING PNT Movement System
-            # ant.theta = (ant.theta + action[i][A_ROT]) % (2* m.pi)
             ant.x += dist * m.cos(ant.theta)
             ant.y += dist * m.sin(ant.theta)
 
+            self.board_bounds(ant)
 
             (x,y) = (int(ant.x), int(ant.y))
 
@@ -116,13 +130,35 @@ class Board(object):
             tile_f /= 2
             if tile_f > 20.0:
                 tile_f -= 5.0
-                ant.food -= 100.0
+                ant.food -= 10.0
             else:
-                ant.food -= 95.0
+                ant.food -= 5.0
             self.food[x][y] = tile_f
 
+            # kill ant if it has no food
+
+
+        ant_list = []
+        ## split / kill
+        for (i,ant) in enumerate(self.Ants):
+            if ant.food > 0:
+                ant_list.append(ant)
+                if ant.food > SPLIT_FOOD:
+                    new_ant = Ant(ant.x, ant.y, np.random.uniform(0, m.pi * 2),
+                              _food = ant.food / 2)
+                    ant_list.append(new_ant)
+                    ant.food /= 2.0
+        self.Ants = ant_list
+        NUM_ANTS = len(self.Ants)
+
+        self.obs   = np.zeros((NUM_ANTS, OBS_LEN),dtype=float)
+        # get obs for each ants
+        # ensure ants within board
+        for (i,ant) in enumerate(self.Ants):
             self.board_bounds(ant)
             self.get_obs(i, ant)
+
+
         return self.obs.copy()
 
 
@@ -132,15 +168,16 @@ class Board(object):
             return ang % (2 * m.pi)
 
 class Ant(object):
-    def __init__(self, x_pos=DIM/2, y_pos=DIM/2, _theta=0, _ID=0):
+    def __init__(self, x_pos=DIM/2, y_pos=DIM/2, _theta=0, _ID=0, _food=50):
         self.x = x_pos + np.random.normal(0, DIM/20)
         self.y = y_pos + np.random.normal(0, DIM/20)
         self.theta = _theta # 0 to 2pi
 
 
+
         self.dx = 0
         self.dy = 0
 
-        self.food = 100.0
+        self.food = _food
 
         self.ID = _ID
